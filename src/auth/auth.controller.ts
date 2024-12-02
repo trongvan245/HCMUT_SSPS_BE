fd58b2898504f23b2f8d9a65f0692bf35cadbed9
Bui Trong Van <trongvan159357@gmail.com>
2024-12-02 21:55:02 +0700

    update swagger api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,20 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
+@ApiTags("authenticate")
 @Controller("auth")
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiOperation({ summary: "Ping" })
   @Get("ping")
   ping() {
     return { msg: "Hello World" };
   }
 
+  @ApiOperation({ summary: "Signup" })
   @Post("signup")
   async signup(@Body() dto: AuthDto) {
     const user = await this.authService.signup(dto);
@@ -18,6 +22,7 @@ export class AuthController {
     // return await this.authService.signin(body);
   }
 
+  @ApiOperation({ summary: "Signin" })
   @Post("signin")
   async signin(@Body() dto: AuthDto) {
     const user = await this.authService.signin(dto);
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,34 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true, forbidNonWhitelisted: false }));
 
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle("Cats example")
+    .setDescription("The cats API description")
+    .setVersion("1.0")
+    .addTag("authenticate")
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document); //https://github.com/nestjs/swagger/issues/92 add trailing slash to the end of the url
+
+  const allowedOrigins = [
+    "http://localhost:5173", // Frontend service 1 (development)
+    "http://localhost:3000", // Frontend service 2 (development)
+    "http://localhost:4000", // Frontend service 3 (development)
+  ];
+
+  app.enableCors({ origin: allowedOrigins, methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS", credentials: true });
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>("PORT") || 4000;
+  await app.listen(port);
 
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
