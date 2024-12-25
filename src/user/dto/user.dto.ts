699455570e4f878ec38fde14189f5eec51ee4876
Bui Trong Van <trongvan159357@gmail.com>
2024-12-25 11:05:10 +0700

    fix: update apiproperty

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,7 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsInt, Min } from "class-validator";
 
 export class PurchasePagesDto {
-    @IsInt()
-    @Min(1)
-    pages: number;
+  @ApiProperty({ description: "Number of pages to purchase", example: 10 })
+  @IsInt()
+  @Min(1)
+  pages: number;
 }
